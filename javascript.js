16763dabcfb4ca730738196eedd356f8295cc46a
Mateen <zahidx297@yahoo.com>
2022-09-18 05:39:43 -0400

    validate matching passwords with js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+const error = document.querySelector('.error-text');
+const password1 = document.querySelector('#password');
+const password2 = document.querySelector('#confirm_password');
+
+document.addEventListener('keyup', run);
+
+function run() {
+    let x = password1.value;
+    let y = password2.value;
+    if (x === y) {
+        error.setAttribute('hidden', 'true');
+    } else {
+        error.removeAttribute('hidden');
+    }
+}
